a94f9b0e6388ac4cd29d12d2ed96222f0fdf4463
DeividBorges93 <deividmborges@gmail.com>
2022-09-20 19:08:24 -0300

    feat(migrations): :sparkles: Create Products migration
    fields: id, name, price and urlImage

---
+++ back-end/src/database/migrations/20220920192616-create-products.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      url_image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('products');
+  }
+};
