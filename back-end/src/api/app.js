764b694af40e6e5f9a666d0356125a4e33225189
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-22 03:45:25 -0300

    feat(assets): :sparkles: access images statically on the server
    it is possible to access images statically on the server through the public folder

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const app = express();
+app.use(express.static('public'));
+app.use('/images',express.static(__dirname + '/public/images'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
