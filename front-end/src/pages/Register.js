f803768f847d72697054af7565152425ae60421e
DeividBorges93 <deividmborges@gmail.com>
2022-09-22 19:37:57 -0300

    feat(pages): :art: create forms (inputs and button)
    input: name, email and password
    
    button: submit (cadastrar)

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,39 +1,48 @@
 import React from 'react';
-import Input from '../components/inputs';
 
 export default function Register() {
   return (
-    <section>
-      <form>
-        <Input
-          type="text"
-          data-testid="common_register__input-name"
-          id="name"
-          name="name"
-          placeholder="Seu nome"
-        />
-        <Input
-          type="email"
-          data-testid="common_register__input-email"
-          id="email"
-          name="email"
-          placeholder="seu-email@site.com.br"
-        />
-        <Input
-          type="password"
-          data-testid="common_register__input-password"
-          id="password"
-          name="password"
-          placeholder="***********"
-        />
-        <button
-          data-testid="common_register__button-register"
-          type="submit"
-        >
-          Cadastrar
+    <div>
+      <h1>Cadastro</h1>
 
-        </button>
-      </form>
-    </section>
+      <section>
+        <form onSubmit={ addUser }>
+          <input
+            type="text"
+            data-testid="common_register__input-name"
+            id="name"
+            name="name"
+            placeholder="Seu nome"
+            onChange={ valueInput }
+            value={ user.name }
+          />
+          <input
+            type="email"
+            data-testid="common_register__input-email"
+            id="email"
+            name="email"
+            placeholder="seu-email@site.com.br"
+            onChange={ valueInput }
+            value={ user.email }
+          />
+          <input
+            type="password"
+            data-testid="common_register__input-password"
+            id="password"
+            name="password"
+            placeholder="***********"
+            onChange={ valueInput }
+            value={ user.password }
+          />
+          <button
+            data-testid="common_register__button-register"
+            type="submit"
+          >
+            Cadastrar
+
+          </button>
+        </form>
+      </section>
+    </div>
   );
 }
