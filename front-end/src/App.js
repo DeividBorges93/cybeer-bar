565107d4919b426fdbe35f631a1c89f64fb75f5d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-22 22:24:02 -0300

    feat(context): :sparkles: wraps components to use context
    for now only on the customer products screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ShoppingCartProvider from './contexts/ShoppingCartProvider.context';
 import CostumerProducts from './pages/CostumerProducts.pages';
 
 function App() {
   return (
     <Routes>
-      <Route path="/customer/products" element={ <CostumerProducts /> } />
+      <Route
+        path="/customer/products"
+        element={
+          <ShoppingCartProvider>
+            <CostumerProducts />
+          </ShoppingCartProvider>
+        }
+      />
     </Routes>
   );
 }
