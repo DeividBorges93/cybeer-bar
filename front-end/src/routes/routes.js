228baf1e809c1c94f15cc003f0b749bb1e39a2c2
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-23 15:54:05 -0300

    refactor: :recycle: moved customer product route
    for the routes component

--- front-end/src/routes/routes.js
+++ front-end/src/routes/routes.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import ShoppingCartProvider from '../contexts/ShoppingCartProvider.context';
 import Register from '../pages/Register';
+import CostumerProducts from '../pages/CostumerProducts.pages';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={ <Register /> } path="/register" />
+        <Route
+          path="/customer/products"
+          element={
+            <ShoppingCartProvider>
+              <CostumerProducts />
+            </ShoppingCartProvider>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
