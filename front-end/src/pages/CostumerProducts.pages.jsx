8f252868c7d3887992ed75f27a2a9d84b95340f7
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-22 22:24:36 -0300

    feat(component): :sparkles: show button to view shopping cart items
    using the shopping cart context

--- front-end/src/pages/CostumerProducts.pages.jsx
+++ front-end/src/pages/CostumerProducts.pages.jsx
@@ -1,19 +1,38 @@
+import { useContext, useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import ProductCard from '../components/ProductCard';
+import { ShoppingCartContext } from '../contexts/ShoppingCartProvider.context';
 import { products } from '../utils/dataMock.util';
 
 function CostumerProducts() {
+  const { totalPrice } = useContext(ShoppingCartContext);
+  const [sowShoppingCart, setShowShoppingCart] = useState(false);
+
+  useEffect(() => {
+    setShowShoppingCart(totalPrice > 0);
+  }, [totalPrice]);
+
+  console.log(totalPrice);
   return (
     <div>
       <Navbar />
       {
-        products.map(({ name, price, urlImage }, index) => (
+        products.map((product, index) => (
           <ProductCard
-            key={ `${name}-${index}` }
-            { ...{ name, price, urlImage, index } }
+            key={ `${product.name}-${index}` }
+            product={ product }
           />
         ))
       }
+
+      { sowShoppingCart && (
+        <button
+          type="button"
+          data-testid="customer_products__checkout-bottom-value"
+        >
+          {`Ver Carrinho: R$${(totalPrice.toFixed(2))}`}
+        </button>
+      )}
     </div>
   );
 }
