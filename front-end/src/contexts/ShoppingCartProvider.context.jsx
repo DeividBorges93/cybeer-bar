61273499065cfc0710ef2e6dfc5e1a8f2e9b3cac
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-26 02:04:44 -0300

    feat(context): :sparkles: handle shopping cart items
    add, remove and update items as well as get the full cart price

--- front-end/src/contexts/ShoppingCartProvider.context.jsx
+++ front-end/src/contexts/ShoppingCartProvider.context.jsx
@@ -4,9 +4,40 @@ import PropTypes from 'prop-types';
 export const ShoppingCartContext = createContext();
 
 function ShoppingCartProvider({ children }) {
-  const [totalPrice, setTotalPrice] = useState(0);
+  const [items, setItems] = useState([]);
 
-  const context = useMemo(() => ({ totalPrice, setTotalPrice }), [totalPrice]);
+  const getTotalPrice = () => (
+    items.reduce((acc, value) => acc + (value.price * value.quantity), 0)
+  );
+  const removeItem = (product) => setItems((prev) => prev
+    .filter((item) => item.name !== product.name));
+  const addItem = (product) => setItems([...items, product]);
+  const getItem = (product) => items.find(({ name }) => name === product.name);
+  const updateItem = (product) => {
+    setItems(items.map((item) => (item.name === product.name ? product : item)));
+  };
+  const handleItem = (product) => {
+    const { quantity } = product;
+    const exist = getItem(product);
+
+    if (exist) {
+      if (quantity === 0) removeItem(product);
+      else updateItem(product);
+    } else {
+      addItem(product);
+    }
+  };
+
+  const context = useMemo(() => ({
+    getTotalPrice,
+    handleItem,
+    addItem,
+    removeItem,
+    updateItem,
+    setItems,
+    getItem,
+    items,
+  }), [items]);
 
   return (
     <ShoppingCartContext.Provider value={ context }>
