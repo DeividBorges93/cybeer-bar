ab4e3182f8073093daee26af4dbcba634902d823
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-26 02:07:28 -0300

    fix(pages): :bug: missing a test id for the cart button
    a data testid for the button and one for the shopping cart text

--- front-end/src/pages/CostumerProducts.pages.jsx
+++ front-end/src/pages/CostumerProducts.pages.jsx
@@ -1,45 +1,56 @@
 import { useContext, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import ProductCard from '../components/ProductCard';
 import { ShoppingCartContext } from '../contexts/ShoppingCartProvider.context';
 import CyBeerBarAPI from '../services/CyBeerBarAPI.service';
 
 function CostumerProducts() {
-  const { totalPrice } = useContext(ShoppingCartContext);
+  const { items, getTotalPrice } = useContext(ShoppingCartContext);
   const [products, setProducts] = useState([]);
-  const [sowShoppingCart, setShowShoppingCart] = useState(false);
+  const [showShoppingCartButton, setShowShoppingCartButton] = useState(true);
+
   const { state } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     localStorage.setItem('user', JSON.stringify(state));
     new CyBeerBarAPI().restoreProducts().then((data) => setProducts(data));
   }, []);
 
-  useEffect(() => {
-    setShowShoppingCart(totalPrice > 0);
-  }, [totalPrice]);
+  useEffect(() => setShowShoppingCartButton(getTotalPrice() === 0), [items]);
 
   return (
     <div>
       <Navbar />
-      {
-        products?.map((product, index) => (
-          <ProductCard
-            key={ `${product.name}-${index}` }
-            product={ product }
-          />
-        ))
-      }
-
-      { sowShoppingCart && (
-        <button
-          type="button"
-          data-testid="customer_products__checkout-bottom-value"
-        >
-          {`Ver Carrinho: R$${(totalPrice.toFixed(2))}`}
-        </button>
-      )}
+      <div style={ { display: 'flex', margin: '50px auto', flexWrap: 'wrap' } }>
+        {
+          products?.map((product, index) => (
+            <div
+              key={ index }
+              style={ {
+                border: '1px solid white',
+                width: '150px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              } }
+            >
+              <ProductCard product={ product } />
+            </div>
+          ))
+        }
+      </div>
+      <button
+        onClick={ () => navigate('/customer/checkout') }
+        type="button"
+        data-testid="customer_products__button-cart"
+        disabled={ showShoppingCartButton }
+      >
+        <span data-testid="customer_products__checkout-bottom-value">
+          {`${getTotalPrice().toFixed(2)}`.replace(/\./, ',')}
+        </span>
+      </button>
     </div>
   );
 }
