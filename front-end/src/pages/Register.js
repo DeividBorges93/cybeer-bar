1ddc557a4cc7f9fa3b4965b4ea8bb0722d2866f9
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-26 06:37:26 -0300

    feat(pages): :sparkles: access to  CyBeerBar API register user
    and changed the way to validate errors in registration fields

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CyBeerBarAPI from '../services/CyBeerBarAPI.service';
 import validate from '../utils/validations';
 
 export default function Register() {
@@ -7,39 +9,49 @@ export default function Register() {
     email: '',
     password: '',
   });
-
-  const [status, setStatus] = useState();
+  const [status, setStatus] = useState([]);
+  const [disableRegisterButton, setDisableRegisterButton] = useState(true);
+  const navigate = useNavigate();
 
   const valueInput = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
   };
 
+  useEffect(() => {
+    setDisableRegisterButton(() => {
+      if (status.length === 0) return true;
+      return !(status.every((err) => !err));
+    });
+  }, [status]);
+
+  useEffect(() => {
+    if (user.email !== '' || user.name !== '' || user.password !== '') {
+      setStatus(validate(user, 'register'));
+    }
+  }, [user]);
+
   const addUser = async (event) => {
     event.preventDefault();
 
     const hasError = validate(user, 'register');
-    setStatus(hasError);
+    const noError = hasError.every((err) => !err);
 
-    // if (hasError.some((error) => !error));
-
-    // const saveDataForm = false; // implementar requisição ao backend
-
-    // if (!saveDataForm) {
-    //   setStatus([{
-    //     type: 'error',
-    //     message: 'Erro: usuário não foi cadastrado!',
-    //   }]);
-    // }
+    if (noError) await new CyBeerBarAPI().register(user, [navigate, setStatus]);
+    else setStatus(hasError);
   };
 
   return (
     <div>
       <h1>Cadastro</h1>
 
-      { status.map((error, index) => (
-        error.type === 'success'
-          ? <p key={ index } style={ { color: 'green' } }>{error.message}</p>
-          : <p key={ index } style={ { color: 'red' } }>{error.message}</p>
+      { status?.map((error, index) => (
+        <p
+          data-testid="common_register__element-invalid_register"
+          key={ index }
+          style={ { color: 'red' } }
+        >
+          {error?.message}
+        </p>
       ))}
 
       <section>
@@ -72,6 +84,7 @@ export default function Register() {
             value={ user.password }
           />
           <button
+            disabled={ disableRegisterButton }
             data-testid="common_register__button-register"
             type="submit"
           >
