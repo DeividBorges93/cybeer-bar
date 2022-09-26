cb96c9edaf9d55d95f6075dc5e6ca43743c6ccda
DeividBorges93 <deividmborges@gmail.com>
2022-09-26 20:16:29 -0300

    feat(pages): :sparkles: implements Checkout page

---
+++ front-end/src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+import TableGenerator from '../components/TableGenerator';
+import Navbar from '../components/Navbar';
+
+export default function CustomerCheckout() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Finalizar Pedidos</h1>
+      <TableGenerator />
+    </div>
+  );
+}
