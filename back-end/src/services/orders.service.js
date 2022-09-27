dc4152b0a6993c515ee9dcd44499e8442c15d48b
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-27 15:22:41 -0300

    refactor: :recycle: sales information return formatting

--- back-end/src/services/orders.service.js
+++ back-end/src/services/orders.service.js
@@ -1,41 +1,25 @@
 const dbModel = require('../database/models');
-
+ 
 class OrdersService {
-  static async createSale({ salesProduct, ...orderInfo }) {
-    const order = await dbModel.Sale.create({
-      totalPrice: orderInfo.totalPrice,
-      deliveryAddress: orderInfo.deliveryAddress,
-      deliveryNumber: orderInfo.deliveryNumber,
-      saleDate: orderInfo.saleDate,
-      status: orderInfo.status,
-    });
-    console.log(order);
-    await OrdersService.createSalesProduct(salesProduct, order.id);
-    return order;
-  }
-
-  static async createSalesProduct(salesProduct, saleId) {
-    const ordersPromises = salesProduct.map((product) => (
-      dbModel.SalesProducts.create({
-        saleId,
-        productId: product.id,
-        quantity: product.quantity,
-      })
-    ));
-    await Promise.all(ordersPromises);
-  }
+ static async createSale({ salesProduct, ...orderInfo }) {
+   const order = await dbModel.Sale.create({ ...orderInfo });
+   const productsOrder = await OrdersService.createSalesProduct(salesProduct, order.id);
+ 
+   return { ...order.dataValues, productsOrder };
+ }
+ 
+ static async createSalesProduct(salesProduct, saleId) {
+   console.log(salesProduct);
+   const productsOrder = await Promise.all(salesProduct.map((product) => (
+     dbModel.SalesProducts.create({
+       saleId,
+       productId: product.productId,
+       quantity: product.quantity,
+     })
+   )));
+ 
+   return productsOrder;
+ }
 }
-
+ 
 module.exports = OrdersService;
-
-//  {
-//   salesProduct: [
-//     {productId, quantity},
-//   ],
-//   userId,
-//   sellerId,
-//   deliveryAddress,
-//   deliveryNumber,
-//   status,
-//   saleDate,
-// }
