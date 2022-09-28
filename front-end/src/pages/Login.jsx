a9da1a3b1e92528cf7859d2a1c9f3024435cf50a
Yuut <ingride.reis.b@gmail.com>
2022-09-28 16:51:56 -0300

    feat(styles): :lipstick: Updating login page styling

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import CyBeerBarAPI from '../services/CyBeerBarAPI.service';
-import '../style/login.css';
+import './style/login.css';
 import routesByRole from '../utils/routesByRole';
 
 export default function Login() {
