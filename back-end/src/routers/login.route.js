8c5c3ac5b2779a71084ce6c2a0787881ce2af03c
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-27 21:20:14 -0300

    feat: :sparkles: endpoint to fetch sellers

--- back-end/src/routers/login.route.js
+++ back-end/src/routers/login.route.js
@@ -6,6 +6,7 @@ const router = Router();
 router
   .post('/register', (req, res) => LoginController.register(req, res))
   .post('/login', (req, res) => LoginController.login(req, res))
+  .get('/sellers', (req, res) => LoginController.getSellers(req, res))
   .post('/decrypt-token', (req, res) => LoginController.decryptToken(req, res));
 
 module.exports = router;
