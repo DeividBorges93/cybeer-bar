3dee3a8d3dbeafdbefff764017a2a28c1a10a978
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-28 02:54:15 -0300

    feat(component): :sparkles: add link to redirect
    go to order details page by clicking on the card

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function OrderCard({ order }) {
   const { id, status, saleDate, totalPrice } = order;
 
   return (
-    <div>
+    <Link to={ `/customer/orders/${id}` }>
       <p data-testid={ `customer_orders__element-order-id-${id}` }>{id}</p>
       <p data-testid={ `customer_orders__element-delivery-status-${id}` }>
         {status}
@@ -13,8 +14,10 @@ function OrderCard({ order }) {
       <p data-testid={ `customer_orders__element-order-date-${id}` }>
         {new Date(saleDate).toLocaleDateString('pt-BR')}
       </p>
-      <p data-testid={ `customer_orders__element-card-price-${id}` }>{totalPrice}</p>
-    </div>
+      <p data-testid={ `customer_orders__element-card-price-${id}` }>
+        {totalPrice.replace('.', ',')}
+      </p>
+    </Link>
   );
 }
 
