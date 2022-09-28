c24a7d345b86b93d46b4b5f4f04127ed7d727581
Ju_M <jm.mnotti@gmail.com>
2022-09-28 20:55:02 -0300

    feat(pages): :sparkles: shows the order details from de customer

--- front-end/src/pages/OrderDetail.jsx
+++ front-end/src/pages/OrderDetail.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+// import OrderProducts from '../components/OrderProducts';
 import Navbar from '../components/Navbar';
+import CyBeerBarAPI from '../services/CyBeerBarAPI.service';
 
 export default function OrderDetail() {
+  const [order, setOrder] = useState({});
+
+  const { id } = useParams();
+  useEffect(() => {
+    new CyBeerBarAPI().getOrdersDetails(id).then(setOrder);
+  }, []);
+
+  console.log(order);
   return (
-    <>
+    <div>
       <Navbar />
-      <p>OrderDetail page</p>
-    </>
+      <h1>Detalhes do Pedido</h1>
+      <p>
+        <span data-testid="customer_order_details__element-order-details-label-order-id">
+          {order.id}
+        </span>
+        <span
+          data-testid="customer_order_details__element-order-details-label-seller-name"
+        >
+          {order.sellers?.name}
+        </span>
+        <span
+          data-testid="customer_order_details__element-order-details-label-order-date"
+        >
+          {new Date(order.saleDate).toLocaleDateString('pt-BR')}
+        </span>
+        <span
+          data-testid={
+            `customer_order_details__element-order-details-label-delivery-status${0}`
+          }
+        >
+          {order.status}
+        </span>
+        <button
+          type="button"
+          data-testid="customer_order_details__button-delivery-check"
+        >
+          MARCAR COMO ENTREGUE
+        </button>
+      </p>
+      {/* <OrderProducts /> */}
+      <p data-testid="customer_order_details__element-order-total-price">
+        {order.totalPrice}
+      </p>
+    </div>
   );
 }
