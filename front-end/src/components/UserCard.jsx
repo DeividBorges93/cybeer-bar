6af2fb74e09101c395e758235d4374e113511902
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-30 19:48:55 -0300

    fix(component): :bug: missing data-testids

--- front-end/src/components/UserCard.jsx
+++ front-end/src/components/UserCard.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 
-function UserCard({ data }) {
-  const { iten, name, email, type } = data;
+function UserCard({ data }, index) {
+  const { item, name, email, type } = data;
 
   return (
     <tr>
-      <th data-testid="69">{iten}</th>
-      <th data-testid="70">{name}</th>
-      <th data-testid="71">{email}</th>
-      <th data-testid="72">{type}</th>
-      <th data-testid="73">Excluir</th>
+      <th
+        data-testid={ `admin_manage__element-user-table-item-number-${index}` }
+      >
+        {item}
+      </th>
+      <th data-testid={ `admin_manage__element-user-table-name-${index}` }>{name}</th>
+      <th data-testid={ `admin_manage__element-user-table-email-${index}` }>{email}</th>
+      <th data-testid={ `admin_manage__element-user-table-role-${index}` }>{type}</th>
+      <th data-testid={ `admin_manage__element-user-table-remove-${index}` }>Excluir</th>
     </tr>
   );
 }
@@ -17,10 +21,11 @@ function UserCard({ data }) {
 UserCard.propTypes = {
   data: PropTypes.shape({
     name: PropTypes.string,
-    iten: PropTypes.number,
+    item: PropTypes.number,
     email: PropTypes.string,
     type: PropTypes.string,
   }),
+  index: PropTypes.number,
 }.isRequired;
 
 export default UserCard;
