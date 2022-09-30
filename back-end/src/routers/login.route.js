74bb60aa17ecaebe3adbaab560f099de6a48620d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-30 18:19:09 -0300

    feat: :sparkles: admin user register route

--- back-end/src/routers/login.route.js
+++ back-end/src/routers/login.route.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const LoginController = require('../controllers/login.controller');
-
+const validateToken = require('../middlewares/validateToken.middleware');
 const router = Router();
 
 router
+  .post('/admin', validateToken, (req, res) => LoginController.register(req, res))
   .post('/register', (req, res) => LoginController.register(req, res))
   .post('/login', (req, res) => LoginController.login(req, res))
   .get('/sellers', (req, res) => LoginController.getSellers(req, res))
